d4d7abbb87a6c3038acad265855a6c84983ef706
Xavier1999-Chen <60979948+Xavier1999-Chen@users.noreply.github.com>
2024-04-07 02:04:59 +0800

    Update upload_file.py

--- pages/upload_file.py
+++ pages/upload_file.py
@@ -4,7 +4,13 @@ st.sidebar.title("导航")
 st.sidebar.page_link("pages/upload_file.py", label="上传文件")
 st.sidebar.page_link("pages/chatbot.py", label="聊天窗口")
 
-st.write("这是个人资料页面")
+# st.write("这是个人资料页面")
+
+uploaded_files = st.file_uploader("Choose a PDF file", accept_multiple_files=False, type=['pdf'])
+if uploaded_file is not None:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    # st.write(bytes_data)
 
 if st.sidebar.button("Logout"):
     st.switch_page("login.py")
